756954a965bcc9e144c3ccfff20dabee08c56d5c
Neetesh-6 <114156149+Neetesh-6@users.noreply.github.com>
2022-11-24 16:46:46 +0530

    Add files via upload

---
+++ junitexamplemanagment/src/main/java/com/junitexample/junitexamplemanagment/App.java
@@ -0,0 +1,13 @@
+package com.junitexample.junitexamplemanagment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
